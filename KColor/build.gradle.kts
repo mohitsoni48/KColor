5219171241081317805cc37ee252891b202bcbbb
Mohit Soni <mohitsoni48@gmail.com>
2024-09-01 10:31:03 +0530

    [Version - 1.0.0.alpha1] - First release

--- KColor/build.gradle.kts
+++ KColor/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    id ("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.28.0"
+
 }
 
 kotlin {
@@ -48,18 +51,48 @@ class KColorPlugin : Plugin<Project> {
 
 apply<KColorPlugin>()
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "com.github.mohitsoni48"
-            artifactId = "KColor"
-            version = "1.0.0.alpha1"
+mavenPublishing {
+    // Define coordinates for the published artifact
+    coordinates(
+        groupId = "io.github.mohitsoni48",
+        artifactId = "KColor",
+        version = "1.0.0.alpha1"
+    )
 
-//            afterEvaluate {
-//                from(components["KColor"])
-//            }
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("KColor")
+        description.set("A library to share color between commonMain and ios swift")
+        inceptionYear.set("2024")
+        url.set("https://github.com/mohitsoni48/KColor")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("mohitsoni48")
+                name.set("Mohit Soni")
+                email.set("mohitsoni48@gmail.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/mohitsoni48/KColor")
         }
     }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
 
 //kColor {
